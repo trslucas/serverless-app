66f529e7a85167db51f3e0eac1330b9165b3f05d
Lucas Trindade <lucastrindade@MacBook-Air-de-Lucas.local>
2024-10-04 13:29:47 -0300

    config: ApiGateway integration with lambda and start log configurations

---
+++ lib/ecommerceApi.stack.ts
@@ -0,0 +1,42 @@
+import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
+import * as cwlogs from 'aws-cdk-lib/aws-logs'
+
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJs.NodejsFunction
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
+    super(scope, id, props)
+
+    const logGroup = new cwlogs.LogGroup(this, 'ECommerceApiLogs')
+    const api = new apiGateway.RestApi(this, 'ECommerceApi', {
+      restApiName: 'ECommerceApi',
+      deployOptions: {
+        accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
+    })
+
+    const productsFetchIntegration = new apiGateway.LambdaIntegration(
+      props.productsFetchHandler,
+    )
+
+    // "/products"
+    const productsResource = api.root.addResource('products')
+    productsResource.addMethod('GET', productsFetchIntegration)
+  }
+}
