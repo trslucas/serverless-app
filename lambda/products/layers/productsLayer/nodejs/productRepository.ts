36acbcb69527fe67cdc93f9ca4749ef83122d55b
Lucas Trindade <lucastrindade@MacBook-Air-de-Lucas.local>
2024-10-26 13:08:14 -0300

    feature: initial crud operations for dynamodb products table

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -1,3 +1,4 @@
+import { DynamoReturnValues } from 'aws-cdk-lib/aws-stepfunctions-tasks'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { v4 as uuid } from 'uuid'
 
@@ -16,5 +17,62 @@ export class ProductRepository {
   constructor(ddbClient: DocumentClient, productsDdb: string) {
     this.ddbClient = ddbClient
     this.productsDdb = productsDdb
+  }
+
+  async getAllProducts(): Promise<Product[]> {
+    const data = await this.ddbClient
+      .scan({
+        TableName: this.productsDdb,
+      })
+      .promise()
+
+    return data.Items as Product[]
+  }
+
+  async getProductById(productId: string): Promise<Product> {
+    const data = await this.ddbClient
+      .get({
+        TableName: this.productsDdb,
+        Key: {
+          id: productId,
+        },
+      })
+      .promise()
+
+    if (data.Item) {
+      return data.Item as Product
+    } else {
+      throw new Error('Product not found')
+    }
+  }
+
+  async create(product: Product): Promise<Product> {
+    product.id = uuid()
+    await this.ddbClient
+      .put({
+        TableName: this.productsDdb,
+        Item: product,
+      })
+      .promise()
+
+    return product
+  }
+
+  async deleteProduct(productId: string): Promise<Product> {
+    const data = await this.ddbClient
+      .delete({
+        TableName: this.productsDdb,
+        Key: {
+          id: productId,
+        },
+        ReturnValues: 'ALL_OLD',
+      })
+      .promise()
+
+    if (data.Attributes) {
+      return data.Attributes as Product
+    } else {
+      throw new Error('Product not foundf')
+    }
   }
 }
